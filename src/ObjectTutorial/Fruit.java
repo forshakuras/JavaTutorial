ff0e41f557dfa665a5a1110bf7c81c90843fead6
Allen Chen <forshakuras@gmail.com>
2022-07-01 19:54:39 -0700

    add object

---
+++ src/ObjectTutorial/Fruit.java
@@ -0,0 +1,41 @@
+package ObjectTutorial;
+
+public class Fruit {
+    private String name;
+    private String colour;
+    private String taste;
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+
+    public Fruit(String name, String colour, String taste) {
+        this.name = name;
+        this.colour = colour;
+        this.taste = taste;
+    }
+
+    public void ShowInformation() {
+        System.out.println(String.format("The fruit %s has the colour %s and tastes %s", name, colour, taste));
+    }
+}
